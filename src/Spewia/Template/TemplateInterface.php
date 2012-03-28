831a5461758034aacae4ffba82abfdb31f863260
Roger Llopart Pla <lumbendil@gmail.com>
2012-03-28 13:52:16 +0200

    Removed a function wich wasn't needed in the interface.

--- src/Spewia/Template/TemplateInterface.php
+++ src/Spewia/Template/TemplateInterface.php
@@ -18,12 +18,6 @@ interface TemplateInterface
     public function setTemplateFile($file);
 
     /**
-     * Sets the layout to use with this template.
-     * @param String $file
-     */
-    public function setLayoutFile($file);
-
-    /**
      * Returns the rendered template.
      *
      * @return String
