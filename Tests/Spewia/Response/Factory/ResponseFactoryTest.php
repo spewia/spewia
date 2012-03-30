b6bab6a131cdb246eae2d50dd3182afd6b0bc452
Roger Llopart Pla <lumbendil@gmail.com>
2012-03-30 11:05:03 +0200

    Fixed some errors in the test.

---
+++ Tests/Spewia/Response/Factory/ResponseFactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Spewia\Response\Factory;
+
+use Spewia\Response\Factory\ResponseFactory;
+
+/**
+ * Tests for the ResponseFactory.
+ */
+class ResponseFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ResponseFactory
+     */
+    protected $object;
+
+    public function setUp()
+    {
+        $this->object = new ResponseFactory;
+    }
+
+    public function testBuild()
+    {
+        $response = $this->object->build();
+
+        $this->assertInstanceOf( '\Spewia\Response\Response' ,$response,
+            'The returned object is an instance of Response.');
+
+        $this->assertEquals('', $response->getContent(),
+            'The content should be empty.');
+
+        $this->assertEquals(200, $response->getStatusCode(),
+            'The response status should be 200.');
+    }
+}
