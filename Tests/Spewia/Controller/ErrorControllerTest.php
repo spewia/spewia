0a7c233758a1e2cc46343474789923e26be5d25a
Aitor Suso <patxi1980@gmail.com>
2012-04-03 18:46:45 +0200

    Added the ErrorController.

--- Tests/Spewia/Controller/ErrorControllerTest.php
+++ Tests/Spewia/Controller/ErrorControllerTest.php
@@ -3,22 +3,86 @@
 namespace Tests\Spewia\Controller;
 
 use Spewia\Controller\ErrorController;
+use Spewia\Exception\Exception;
 
 class ErrorControllerTests extends \PHPUnit_Framework_TestCase
 {
+    protected $template;
+    protected $container;
+    protected $response;
+    protected $controller;
+    protected $exception;
 
     public function setUp()
     {
+        $this->template = \Mockery::mock('Spewia\Template\TemplateInterface');
 
+        $this->response = \Mockery::mock('Symfony\Component\HttpFoundation\Response');
+        $factory = \Mockery::mock('Spewia\Factory\FactoryInterface');
+
+        $this->container = \Mockery::mock('Spewia\DependencyInjection\ContainerInterface');
+
+        $this->container
+        ->shouldReceive('get')
+        ->with('factory.response')
+        ->andReturn($factory);
+
+        $factory
+        ->shouldReceive('build')
+        ->andReturn($this->response);
+
+        $this->container
+        ->shouldReceive('get')
+        ->with('template')
+        ->andReturn($this->template);
+
+        $this->exception = new Exception();
+
+        $this->controller = new ErrorController($this->container);
     }
 
     public function testError404()
     {
-        $this->markTestIncomplete();
+        $this->template
+        ->shouldReceive('render')
+        ->once();
+
+        $this->response
+        ->shouldReceive('setContent')
+        ->atLeast(1);
+
+        $this->response
+        ->shouldReceive('setStatusCode')
+        ->with('404');
+
+        $this->controller->error404Action($this->exception);
+        $response = $this->controller->render();
+
+        $this->assertSame($this->response, $response,
+            'The response is not the same as the returned by the controller.'
+        );
     }
 
     public function testError5xx()
     {
-        $this->markTestIncomplete();
+        $this->template
+        ->shouldReceive('render')
+        ->once();
+
+        $this->response
+        ->shouldReceive('setContent')
+        ->atLeast(1);
+
+        $this->response
+        ->shouldReceive('setStatusCode')
+        ->with('500');
+
+
+        $this->controller->error5xxAction($this->exception);
+        $response = $this->controller->render();
+
+        $this->assertSame($this->response, $response,
+            'The response is not the same as the returned by the controller.'
+        );
     }
 }
--- src/Spewia/Controller/ErrorController.php
+++ src/Spewia/Controller/ErrorController.php
@@ -2,15 +2,18 @@
 
 namespace Spewia\Controller;
 
+use Spewia\Exception\Exception;
+
 class ErrorController extends BaseController
 {
-    public function error404Action()
-    {
 
+    public function error404Action(Exception $exception)
+    {
+        $this->response->setStatusCode(404);
     }
 
-    public function error5xxAction()
+    public function error5xxAction(Exception $exception)
     {
-
+        $this->response->setStatusCode(500);
     }
 }
